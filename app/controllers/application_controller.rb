28f6a7983a4caa7b4666130a5b0caa5c7494ea66
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-18 17:25:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/recipes/new' do
+    erb :new
+  end
+
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
@@ -19,7 +23,5 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
-  get '/recipes/new' do
-    erb :new
-  end
+  
 end
